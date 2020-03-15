c10b1a4b47d75aca1a99714c1243da0bb2049e33
Anton <antonoutkine@gmail.com>
2020-03-15 13:18:26 -0500

    Improve structure and begin work on tilemap

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,16 @@ use nphysics2d::joint::DefaultJointConstraintSet;
 use nphysics2d::math::{Inertia, Velocity};
 use nphysics2d::object::{
     Body, BodyHandle, BodyPart, BodyPartHandle, BodySet, BodyStatus, ColliderDesc, ColliderHandle,
-    ColliderSet, DefaultBodyHandle, DefaultBodySet, DefaultColliderSet, RigidBodyDesc,
+    ColliderSet, DefaultBodyHandle, DefaultBodySet, DefaultColliderSet, Ground, RigidBody,
+    RigidBodyDesc,
 };
 use nphysics2d::world::{
     DefaultGeometricalWorld, DefaultMechanicalWorld, GeometricalWorld, MechanicalWorld,
 };
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use tiled::parse;
 
 fn main() {
     // Make a Context.
@@ -43,6 +48,38 @@ struct Player {
     body_handle: DefaultBodyHandle,
 }
 
+impl Player {
+    const X_POWER: f32 = 100.;
+    const SIZE: f32 = 10.;
+
+    pub fn update(&mut self, ctx: &mut Context, physics: &mut Physics) {
+        let x_dir = if keyboard::is_key_pressed(ctx, KeyCode::Left) {
+            -1.
+        } else if keyboard::is_key_pressed(ctx, KeyCode::Right) {
+            1.
+        } else {
+            0.
+        };
+
+        let rigid_body = physics.body_set.rigid_body_mut(self.body_handle).unwrap();
+        rigid_body.set_linear_velocity(Vector2::new(
+            Player::X_POWER * x_dir,
+            rigid_body.velocity().linear[1],
+        ))
+    }
+
+    pub fn draw(&mut self, ctx: &mut Context, physics: &mut Physics) {
+        let rigid_body = physics.body_set.rigid_body(self.body_handle).unwrap();
+        let coords = rigid_body.position() * Point2::origin();
+
+        let rect = graphics::Rect::new(coords.x, coords.y, Player::SIZE, Player::SIZE);
+        let r1 =
+            graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), rect, graphics::BLACK)
+                .unwrap();
+        graphics::draw(ctx, &r1, DrawParam::default()).unwrap();
+    }
+}
+
 struct Physics {
     mechanical_world: DefaultMechanicalWorld<f32>,
     geometrical_world: DefaultGeometricalWorld<f32>,
@@ -52,6 +89,18 @@ struct Physics {
     force_generator_set: DefaultForceGeneratorSet<f32>,
 }
 
+impl Physics {
+    pub fn step(&mut self) {
+        self.mechanical_world.step(
+            &mut self.geometrical_world,
+            &mut self.body_set,
+            &mut self.collider_set,
+            &mut self.joint_constraint_set,
+            &mut self.force_generator_set,
+        );
+    }
+}
+
 struct MyGame {
     player: Player,
     physics: Physics,
@@ -59,6 +108,34 @@ struct MyGame {
 
 impl MyGame {
     pub fn new(_ctx: &mut Context) -> MyGame {
+        let file = File::open(&Path::new("assets/tilemap.tmx")).unwrap();
+        let tilemap = tiled::parse(file).unwrap();
+        let level_size = {
+            let level_size = &["level_width", "level_height"]
+                .iter()
+                .map(|property| {
+                    if let tiled::PropertyValue::IntValue(val) =
+                        tilemap.properties.get(*property).unwrap()
+                    {
+                        *val as usize
+                    } else {
+                        panic!("Tiled property has wrong type!")
+                    }
+                })
+                .collect::<Vec<usize>>();
+            (level_size[0], level_size[1])
+        };
+
+        let levels = tilemap.layers[0]
+            .tiles
+            .iter()
+            .flatten()
+            .copied()
+            .collect::<Vec<u32>>();
+        let levels =
+            na::DMatrix::from_row_slice(tilemap.width as usize, tilemap.height as usize, &levels);
+        println!("{}", levels.slice((0, 0), level_size));
+
         let geometrical_world = DefaultGeometricalWorld::new();
         let gravity = Vector2::y() * 100.;
         let mechanical_world = DefaultMechanicalWorld::new(gravity);
@@ -92,50 +169,15 @@ impl MyGame {
 
 impl EventHandler for MyGame {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        {
-            use KeyCode::*;
-            let player = self
-                .physics
-                .body_set
-                .rigid_body_mut(self.player.body_handle)
-                .unwrap();
-
-            let power = 100.;
-            let x_dir = if keyboard::is_key_pressed(ctx, KeyCode::Left) {
-                -1.
-            } else if keyboard::is_key_pressed(ctx, KeyCode::Right) {
-                1.
-            } else {
-                0.
-            };
-            player.set_linear_velocity(Vector2::new(power * x_dir, player.velocity().linear[1]))
-        }
-
-        let physics = &mut self.physics;
-        physics.mechanical_world.step(
-            &mut physics.geometrical_world,
-            &mut physics.body_set,
-            &mut physics.collider_set,
-            &mut physics.joint_constraint_set,
-            &mut physics.force_generator_set,
-        );
+        self.player.update(ctx, &mut self.physics);
+        self.physics.step();
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, graphics::WHITE);
 
-        let player = self
-            .physics
-            .body_set
-            .rigid_body(self.player.body_handle)
-            .unwrap();
-        let coords = player.position() * Point2::origin();
-
-        let rect = graphics::Rect::new(coords.x, coords.y, 10., 10.);
-        let r1 =
-            graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), rect, graphics::BLACK)?;
-        graphics::draw(ctx, &r1, DrawParam::default())?;
+        self.player.draw(ctx, &mut self.physics);
 
         graphics::present(ctx)
     }
